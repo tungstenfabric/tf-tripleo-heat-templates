e4179bca9ca290e6ced784ffe308dfba761b1d30
alexey-mr <alexey.morlang@gmail.com>
2020-09-21 15:36:19 +0300

    Added tasks for ffu
    - added external tasks for stopping contrail services
    - added system_upgrade tasks for removal of hugepages mounts from fstat
    otherwise leapp upgrade hangs
    - added sys upgrade tasks for removal sriob as leapp doesnt work
    correctly with VFs
    - use ansible var instead of harcoded docker/podman
    - remove 'docker' related stuff
    
    partial-jira-bug: CEM-17918
    Change-Id: Ia3d66b8921bd52db965c3b614df178803ff9c8b6

--- tools/contrail/stop_contrail_api_containers.sh
+++ tools/contrail/stop_contrail_api_containers.sh
@@ -7,10 +7,10 @@ STOP_CONTAINERS=${STOP_CONTAINERS:-'contrail_config_api contrail_analytics_api'}
 
 #Detecting containers to stop
 for container in $STOP_CONTAINERS; do
-    check=$(sudo docker ps --format '{{.Names}}' | grep -c "$container")
+    check=$(sudo podman ps --format '{{.Names}}' | grep -c "$container")
     if [ $check -gt 0 ]; then
        echo Found container $container. Stopping
-       sudo docker stop $container
+       sudo podman stop $container
     fi
 done
 
