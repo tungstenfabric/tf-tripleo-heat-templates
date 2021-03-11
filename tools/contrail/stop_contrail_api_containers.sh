794abc4522ae434791dc339629a23c5b5e5d94a2
Gleb Galkin <gleb108@gmail.com>
2021-03-11 17:16:32 +0300

    Stop agent and update kernel module
    Fixed .gitreview
    partial-jira-bug: CEM-20980
    
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: Iae0d6628a54b219038ee9244446adec4157b21d7

--- tools/contrail/stop_contrail_api_containers.sh
+++ tools/contrail/stop_contrail_api_containers.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+#In OSP16 podman containers are managed by systemctl services. 
+#list of services that must be stopped before update procedure
+STOP_SERVICES=${STOP_SERVICES:-'contrail_config_api contrail_analytics_api'}
 
-#list containers that must be stopped before update procedure
-STOP_CONTAINERS=${STOP_CONTAINERS:-'contrail_config_api contrail_analytics_api'}
 
-
-#Detecting containers to stop
-for container in $STOP_CONTAINERS; do
-    check=$(sudo podman ps --format '{{.Names}}' | grep -c "$container")
+#Detecting services to stop
+for service in $STOP_SERVICES; do
+    check=$(sudo systemctl -a | cut -d ' ' -f3 | grep -c "$service")
     if [ $check -gt 0 ]; then
-       echo Found container $container. Stopping
-       sudo podman stop $container
+       echo Found service $service. Stopping
+       sudo systemctl stop $service || true
     fi
 done
 
--- tools/contrail/update_contrail_preparation.sh
+++ tools/contrail/update_contrail_preparation.sh
@@ -5,6 +5,7 @@ my_dir="$(dirname $my_file)"
 
 #New contrail version
 CONTRAIL_NEW_IMAGE_TAG=${CONTRAIL_NEW_IMAGE_TAG:-${CONTRAIL_IMAGE_TAG:-'latest'}}
+echo "INFO: CONTRAIL_NEW_IMAGE_TAG=$CONTRAIL_NEW_IMAGE_TAG"
 
 #Prefix for all contrail containers
 CONTRAIL_IMAGE_PREFIX=${CONTRAIL_IMAGE_PREFIX:-'contrail-'}
@@ -22,8 +23,8 @@ SSH_OPTIONS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 #Pull new images on the nodes
 for ip_addr in $(openstack server list -f value -c Networks | cut -d '=' -f 2); do
     echo "node: ${ip_addr}  Pulling new container images:"
-    scp ${SSH_OPTIONS} $my_dir/pull_new_contrail_images.sh ${SSH_USER}@${ip_addr}:/tmp/
-    ssh ${SSH_OPTIONS} ${SSH_USER}@${ip_addr} CONTRAIL_IMAGE_PREFIX=${CONTRAIL_IMAGE_PREFIX} CONTRAIL_NEW_IMAGE_TAG=${CONTRAIL_NEW_IMAGE_TAG} /tmp/pull_new_contrail_images.sh
+    scp ${SSH_OPTIONS} $my_dir/pull_new_contrail_images.sh ${SSH_USER}@${ip_addr}:
+    ssh ${SSH_OPTIONS} ${SSH_USER}@${ip_addr} CONTRAIL_IMAGE_PREFIX=${CONTRAIL_IMAGE_PREFIX} CONTRAIL_NEW_IMAGE_TAG=${CONTRAIL_NEW_IMAGE_TAG} ./pull_new_contrail_images.sh
 done
 
 #Stoping config_api/analytics_api containers
@@ -31,7 +32,7 @@ echo STOPPING CONTRAIL API CONTAINERS
 for ip_addr in $(openstack server list -f value -c Networks | cut -d '=' -f 2); do
     echo "Node: ${ip_addr}  Stopping contrail api containers"
     STOP_CONTAINERS_ESC=$(echo $STOP_CONTAINERS | sed -e 's/ /\\ /g')
-    scp ${SSH_OPTIONS} $my_dir/stop_contrail_api_containers.sh ${SSH_USER}@${ip_addr}:/tmp/
-    ssh ${SSH_OPTIONS} ${SSH_USER}@${ip_addr} STOP_CONTAINERS="${STOP_CONTAINERS_ESC}" /tmp/stop_contrail_api_containers.sh
+    scp ${SSH_OPTIONS} $my_dir/stop_contrail_api_containers.sh ${SSH_USER}@${ip_addr}:
+    ssh ${SSH_OPTIONS} ${SSH_USER}@${ip_addr} STOP_CONTAINERS="${STOP_CONTAINERS_ESC}" ./stop_contrail_api_containers.sh
 done
 
