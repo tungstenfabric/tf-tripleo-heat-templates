a2baee111f8363f2a47e60d6747481c071a109ab
Gleb Galkin <gleb108@gmail.com>
2021-11-17 14:35:43 +0300

    ISSU process for CN21.4 Upgrade
    tools for migrating contrail control plane to openshift
    
    partial-jira-bug: CEM-24701
    Signed-off-by: Gleb Galkin <gleb108@gmail.com>
    Change-Id: I7e4c11c1c086010ac6cd1299086d717c24526c3d

---
+++ tools/contrail/issu/issu_node_pair.sh
@@ -0,0 +1,104 @@
+#!/bin/bash -x
+
+oper=${1}
+if [ -z "$oper" ] ; then
+    echo "ERROR: operation not provided: add or del"
+    exit -1
+fi
+
+if [[ -z "$admin_user" || \
+      -z "$admin_tenant_name" || \
+      -z "$admin_password" || \
+      -z "$router_asn" || \
+      -z "$issu_ips_list_space" || \
+      -z "$issu_control_ips_space" || \
+      -z "$issu_api_server_ip" || \
+      -z "$old_api_server_ip" || \
+      -z "$old_control_servers_list_space" ]] ; then
+   echo "ERRPR: check that admin_user, admin_tenant_name, admin_password, router_asn, issu_ips_list_space, issu_control_ips_space, issu_api_server_ip, old_api_server_ip, old_control_servers_list_space are not empty."
+   exit -1
+fi
+
+for i in $old_control_servers_list_space $issu_control_ips_space ; do
+  if [ -z "${node_name[$i]}" ] ; then
+    echo "ERROR: node_name array doesnt have map ip=>fqdn for $i"
+    exit -1
+  fi
+done
+
+AUTH_PARAMS="--admin_password $admin_password"
+AUTH_PARAMS+=" --admin_tenant_name $admin_tenant_name"
+AUTH_PARAMS+=" --admin_user $admin_user"
+
+asn_opts="--router_asn $router_asn"
+
+bgp_auto_mesh_opts=''
+if [[ ${bgp_auto_mesh,,} == 'true' ]] ; then
+  bgp_auto_mesh_opts="--ibgp_auto_mesh"
+fi
+
+old_api_server_port=${old_api_server_port:-'18082'}
+issu_api_server_port=${issu_api_server_port:-'8082'}
+
+function provision() {
+  local ip=$1
+  shift 1
+  local name=$1
+  shift 1
+  local api=$1
+  shift 1
+  local api_port=$1
+  shift 1
+  local provision_script=$1
+  shift 1
+  local opts="$@"
+
+  python /opt/contrail/utils/${provision_script} --host_name $name \
+    --host_ip $ip \
+    --api_server_ip $api \
+    --api_server_port $api_port \
+    --oper $oper \
+    --api_server_use_ssl true \
+    $AUTH_PARAMS $opts
+  local ret=$?
+  echo "INFO: ${provision_script} --host_name $name ... exit with code $ret"
+  return $ret
+}
+
+function provision_control() {
+  local ip=$1
+  local name=$2
+  local api=$3
+  local port=$4
+
+  provision $ip $name $api $port provision_control.py $asn_opts $bgp_auto_mesh_opts
+}
+
+#Pair/unpair control nodes with issu node.
+for ip in $old_control_servers_list_space ; do
+  provision_control $ip ${node_name[$ip]} $issu_api_server_ip $issu_api_server_port || {
+    echo "ERROR: failed to provision old control node $ip in ISSU cluster $issu_api_server_ip"
+    exit -1
+  }
+done
+
+#Pair/unpair issu control nodes in with cluster
+for ip in $issu_control_ips_space ; do
+  provision_control $ip ${node_name[$ip]} $old_api_server_ip $old_api_server_port || {
+    echo "ERROR: failed to provision ISSU control node $ip in old cluster $old_api_server_ip"
+    exit -1
+  }
+done
+
+if [[ "$oper" == 'del' ]] ; then
+  # remove from issu node old config, analytics & analytics db nodes registered by issu_sync
+  for ip in $old_config_servers_list_space ; do
+    provision $ip ${node_name[$ip]} $issu_api_server_ip $issu_api_server_port provision_config_node.py $config_container_id
+  done
+  for ip in $old_analytics_servers_list_space ; do
+    provision $ip ${node_name[$ip]} $issu_api_server_ip $issu_api_server_port provision_analytics_node.py $analytics_container_id
+  done
+  for ip in $old_analyticsdb_servers_list_space ; do
+    provision $ip ${node_name[$ip]} $issu_api_server_ip $issu_api_server_port provision_database_node.py $analyticsdb_container_id
+  done
+fi
